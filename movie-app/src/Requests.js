c32b73ae45fe702feed6751e787fe0db2b31dcb3
Ansh <ansh.d.gala@gmail.com>
2025-06-11 19:31:18 +0530

    Fixed env variable

--- movie-app/src/Requests.js
+++ movie-app/src/Requests.js
@@ -1,10 +1,8 @@
-const key = process.env.MOVIE_API_KEY;
+const key = process.env.REACT_APP_MOVIE_API_KEY;
 
 const requests = {
-  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=8321fba1bd0a71fd23430a1b4d42bfd9&language=en-US&page=1`,
-  requestSearch:`​https://api.themoviedb.org/3/search/movie?api_key=8321fba1bd0a71fd23430a1b4d42bfd9&language=en-US&page=1&query=`
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestSearch: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&page=1&query=`
 };
-
-console.log(key);
 
 export default requests;
